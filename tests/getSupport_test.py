a80548fe158d5ae40dca26b327a03c0f2dbb9165
marina-fares <marina.fares11@gmail.com>
2020-04-10 15:17:54 +0200

    getSupport version two

--- tests/getSupport_test.py
+++ tests/getSupport_test.py
@@ -25,35 +25,13 @@ def getSupport(df,data):
             support (float): the support
     """
     #make an array with the number of transactions
-    num_of_values=1 # to calculate totla numbers of values multiplied in each others
-    num_of_col=len(data)# number of columns in data list
-    num_of_trans=4 #total number of transactions
-    arr=np.zeros((num_of_col,num_of_trans))#array to get the spesific rows from global data
-    lists=np.array(num_of_col)
-    #ans=np.ones((2,4))#####################################################################
-    for i in range(len(data)):#loop for tuples in the list
-        arr[i]=(np.array(df[data[i][0]]))
-        num_of_values*=len(data[i][1])
-###################################################### this section to merge values in all lists and generate onle list
-    merged_array=np.zeros([len(data),num_of_values])############## this array to merge all values in lists
-    for i in range(len(data)):
-        list_len=(int(num_of_values/len(data[i][1])))
-        arr1=np.full((list_len,len(data[i][1])),data[i][1])
-        arr1=arr1.flatten()
-        merged_array[i]=(arr1)
-    merged_array=merged_array.T
-##############################################################################
     ans=np.ones(4)
-    k=0
-    for i in range(len(merged_array)):
-        for j in range(len(merged_array[i])):
-            if(k==num_of_col):
-                k=0
-            num=(arr[k]==merged_array[i][j])
-            ans=np.logical_and(ans,num)
-            k+=1
-        print(merged_array[i],sum(ans))
-        ans=np.ones(4)
+    for i in range(len(data)):
+        arr=np.array(df[data[i][0]])
+        num=(arr==data[i][1])
+        ans=np.logical_and(ans,num)
+    print(sum(ans))
+    ans=np.ones(4)
 
-data=[('a',[1,2,3]),('b',[8,3]),('c',[0,1])]
-getSupport(df,data)
+data_in=[('a',2),('b',8),('c',0)]
+getSupport(df,data_in)
